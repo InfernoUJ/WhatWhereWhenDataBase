ef2e2f94095889304515ae49e37f2384a298d659
InfernoUJ <mykhailo.petin@student.uj.edu.pl>
2023-06-10 10:48:56 +0200

    last changes

--- dataGeneration/bigFiller.py
+++ dataGeneration/bigFiller.py
@@ -8,6 +8,15 @@ fake = Faker('pl_PL')
 start_date = datetime.date(1965, 1, 1)
 end_date = datetime.date(2023, 6, 3)
 
+# CITIES
+numberOfCities = random.randint(10, 20)
+cities = [None for _ in range(numberOfCities)]
+with open('data/miejscowosci.data', 'w') as f:
+    f.write("COPY miejscowosci FROM STDIN DELIMITER ',' NULL 'null';\n")
+    for i in range(numberOfCities):
+        cities[i] = fake.city()
+        f.write(f"{i},{cities[i]}\n")
+
 
 # ADRESY
 numberOfAddresses = random.randint(20, 40)
@@ -15,7 +24,7 @@ with open('data/adresy.data', 'w') as f:
     f.write("COPY adresy FROM STDIN DELIMITER ',' NULL 'null';\n")
 
     for i in range(numberOfAddresses):
-        f.write(f"{i},{fake.city()},{fake.street_name()},{fake.postcode()},{random.randint(1,40)},{random.randint(1, 100)}\n")
+        f.write(f"{i},{random.randint(0, numberOfCities-1)},{fake.street_name()},{fake.postcode()},{random.randint(1,40)},{random.randint(1, 100)}\n")
     
     f.write("\\.\n")
 
@@ -71,6 +80,8 @@ with open('data/autorzy_pytan.data', 'w') as f:
             l[id_autora-170] += 1
     f.write("\\.\n")
 
+for a in autors:
+    print(a, playersBirthday[a])
 
 # AUTORZY
 with open('data/autorzy.data', 'w') as f:
@@ -105,6 +116,8 @@ with open('data/turnieje.data', 'w') as f:
 
     f.write("\\.\n")
 
+for i, t in enumerate(datesOfTournaments):
+    print(i, t)
 
 # PYTANIA NA TURNIEJACH
 que_tourn = [None]+[_ for _ in range(0, numberOfQuestions)]
@@ -113,17 +126,7 @@ que_to_choose = [i for i in range(numberOfQuestions)]
 with open("data/pytania_na_turniejach.data", "w") as f:
     f.write("COPY pytania_na_turniejach FROM STDIN DELIMITER ';' NULL 'null';\n")
 
-    for j in range(10):
-        for i in range(36):
-            #print(que_to_choose, len(que_to_choose))
-            print(j, i, len([question for question in que_to_choose if datesOfTournaments[j] > playersBirthday[que_aut[question]] + datetime.timedelta(days=365*5)]))
-            que = random.choice([question for question in que_to_choose if datesOfTournaments[j] > playersBirthday[que_aut[question]] + datetime.timedelta(days=365*5)])
-            que_tourn[que] = j
-            tourn_que[j].append(que)
-            que_to_choose.remove(que)
-            f.write(f"{j};{que};{i+1}\n")
-    print("\n\n")
-    for j in range(10, numberOfTournaments):
+    for j in range(numberOfTournaments):
         for i in range(24):
             print(j, i, len([question for question in que_to_choose if datesOfTournaments[j] > playersBirthday[que_aut[question]] + datetime.timedelta(days=365*5)]))
             que = random.choice([question for question in que_to_choose if datesOfTournaments[j] > playersBirthday[que_aut[question]] + datetime.timedelta(days=365*5)])
@@ -135,13 +138,42 @@ with open("data/pytania_na_turniejach.data", "w") as f:
     f.write("\\.\n")
 
 
+# ZESPOLY
+numberOfTeams = random.randint(20, 30)
+with open('data/zespoly.data', 'w') as f:
+    f.write("COPY zespoly FROM STDIN DELIMITER ';' NULL 'null';\n")
+    
+    for i in range(5,numberOfTeams):
+        # Generate a random date between the start and end dates
+        time_between_dates = end_date - start_date
+        days_between_dates = time_between_dates.days
+        random_number_of_days = random.randrange(days_between_dates)
+        random_date = start_date + datetime.timedelta(days=random_number_of_days)
+
+        # Format the date as a string in the format "YYYY-MM-DD"
+        formatted_date = random_date.strftime('%Y-%m-%d')       
+        data_likwidacji = "null"
+        if(random.choice([i for i in range(20)]) == 13):
+            time_between_dates = end_date - random_date
+            days_between_dates = time_between_dates.days
+            random_number_of_days = random.randrange(days_between_dates)
+            data_likwidacji = random_date + datetime.timedelta(days=random_number_of_days)
+            data_likwidacji = data_likwidacji.strftime('%Y-%m-%d')
+        
+        city = random.choice([_ for _ in range(numberOfCities)] + ['null']*3)
+
+        f.write(f"{i};{fake.word().capitalize()+' '+fake.word()};{formatted_date};{data_likwidacji};{city}\n")
+    
+    f.write("\\.\n")
+
+
 # SKLADY W ZESPOLACH
 # ZMIANY
 with (open('data/sklady_w_zespolach.data', 'w') as f, open('data/zmiany.data', 'w') as zm):
     f.write("COPY sklady_w_zespolach FROM STDIN DELIMITER ';' NULL 'null';\n")
     zm.write("COPY zmiany FROM STDIN DELIMITER ';' NULL 'null';\n")
 
-    for turn in range(20):
+    for turn in range(numberOfTournaments):
         available_teams = [i for i in range(0, 20)]
         available_players = [i for i in range(0, 300)]
         for i in range(random.randint(6, 20)):
