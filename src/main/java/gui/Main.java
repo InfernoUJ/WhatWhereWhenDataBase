7bf5b6d006c38ce362fad14385326d8eac5e7bf1
Feq149 <Konrad.ciborowski@student.uj.edu.pl>
2023-06-10 20:11:27 +0200

    main

--- src/main/java/gui/Main.java
+++ src/main/java/gui/Main.java
@@ -3,7 +3,7 @@ package gui;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class WhatWhereWhenArchive extends Application {
+public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
@@ -11,5 +11,6 @@ public class WhatWhereWhenArchive extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         stage.setTitle("Archiwum CoGdzieKiedy?");
+
     }
 }
