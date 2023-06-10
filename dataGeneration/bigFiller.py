18ac609e6d0ad273d67cd9b2b9c08c598214acc0
InfernoUJ <mykhailo.petin@student.uj.edu.pl>
2023-06-10 10:33:46 +0200

    questions for tournaments are generated correctly

--- dataGeneration/bigFiller.py
+++ dataGeneration/bigFiller.py
@@ -21,11 +21,11 @@ with open('data/adresy.data', 'w') as f:
 
 
 # PYTANIA
-numberOfQuestions = random.randint(700, 1000)
+numberOfQuestions = random.randint(3000, 4000)
 with open('data/pytania.data', 'w') as f:
     f.write("COPY pytania FROM STDIN DELIMITER ';' NULL 'null';\n")
 
-    for i in range(13, numberOfQuestions+1):
+    for i in range(12, numberOfQuestions):
         f.write(f"{i};{lorem.words(random.randint(30,50))};{lorem.words(1)};{random.randint(0,10)}\n")
 
     f.write("\\.\n")
@@ -37,7 +37,7 @@ playersBirthday = [None for _ in range(numberOfPlayers)]
 with open('data/uczestnicy.data', 'w') as f:
     f.write("COPY uczestnicy FROM STDIN DELIMITER ';' NULL 'null';\n")
     
-    for i in range(300):
+    for i in range(numberOfPlayers):
         # Generate a random date between the start and end dates
         time_between_dates = end_date - start_date
         days_between_dates = time_between_dates.days
@@ -54,6 +54,24 @@ with open('data/uczestnicy.data', 'w') as f:
     f.write("\\.\n")
 
 
+# AUTORZY PYTAN
+autors = set()
+l = [0 for _ in range(40)] # number of questions for each author
+que_aut = [None for _ in range(numberOfQuestions)]
+with open('data/autorzy_pytan.data', 'w') as f:
+    f.write("COPY autorzy_pytan FROM STDIN DELIMITER ';' NULL 'null';\n")
+    for i in range(numberOfQuestions):
+        id_autora = random.choice([i for i in range(30)]+[i for i in range(200,210)])
+        autors.add(id_autora)
+        que_aut[i] = id_autora
+        f.write(f"{id_autora};{i}\n")
+        if(id_autora < 30):
+            l[id_autora] += 1
+        else:
+            l[id_autora-170] += 1
+    f.write("\\.\n")
+
+
 # AUTORZY
 with open('data/autorzy.data', 'w') as f:
     f.write("COPY autor FROM STDIN DELIMITER ';' NULL 'null';\n")
@@ -61,24 +79,6 @@ with open('data/autorzy.data', 'w') as f:
         f.write(f"{i};{l[i]}\n")
     for i in range(30, 40):
         f.write(f"{i+170};{l[i]}\n")
-    f.write("\\.\n")
-
-
-# AUTORZY PYTAN
-autors = set()
-l = [0 for _ in range(40)] # number of questions for each author
-que_aut = [_ for _ in range(1, 701)]
-with open('data/autorzy_pytan.data', 'w') as f:
-    f.write("COPY autorzy_pytan FROM STDIN DELIMITER ';' NULL 'null';\n")
-    for i in range(1, 701):
-        id_autora = random.choice([i for i in range(30)]+[i for i in range(200,210)])
-        autors.add(id_autora)
-        que_aut[i-1] = id_autora
-        f.write(f"{id_autora};{i}\n")
-        if(id_autora < 30):
-            l[id_autora] += 1
-        else:
-            l[id_autora-170] += 1
     f.write("\\.\n")
 
 
@@ -92,7 +92,7 @@ with open('data/turnieje.data', 'w') as f:
         days_between_dates = time_between_dates.days
         random_number_of_days = random.randrange(days_between_dates)
         random_date = start_date + datetime.timedelta(days=random_number_of_days)
-        datesOfTournaments[i] = formatted_date # !!!!!!!!!!!!
+        datesOfTournaments[i] = random_date # !!!!!!!!!!!!
         formatted_date = random_date.strftime('%Y-%m-%d')    
 
 
@@ -109,21 +109,24 @@ with open('data/turnieje.data', 'w') as f:
 # PYTANIA NA TURNIEJACH
 que_tourn = [None]+[_ for _ in range(0, numberOfQuestions)]
 tourn_que = [list() for _ in range(numberOfTournaments)]
-que_to_choose = [i for i in range(1, numberOfQuestions+1)]
+que_to_choose = [i for i in range(numberOfQuestions)]
 with open("data/pytania_na_turniejach.data", "w") as f:
     f.write("COPY pytania_na_turniejach FROM STDIN DELIMITER ';' NULL 'null';\n")
 
     for j in range(10):
         for i in range(36):
-            que = random.choice(list(que_to_choose))
+            #print(que_to_choose, len(que_to_choose))
+            print(j, i, len([question for question in que_to_choose if datesOfTournaments[j] > playersBirthday[que_aut[question]] + datetime.timedelta(days=365*5)]))
+            que = random.choice([question for question in que_to_choose if datesOfTournaments[j] > playersBirthday[que_aut[question]] + datetime.timedelta(days=365*5)])
             que_tourn[que] = j
             tourn_que[j].append(que)
             que_to_choose.remove(que)
             f.write(f"{j};{que};{i+1}\n")
-
+    print("\n\n")
     for j in range(10, numberOfTournaments):
         for i in range(24):
-            que = random.choice(list(que_to_choose))
+            print(j, i, len([question for question in que_to_choose if datesOfTournaments[j] > playersBirthday[que_aut[question]] + datetime.timedelta(days=365*5)]))
+            que = random.choice([question for question in que_to_choose if datesOfTournaments[j] > playersBirthday[que_aut[question]] + datetime.timedelta(days=365*5)])
             que_tourn[que] = j
             tourn_que[j].append(que)
             que_to_choose.remove(que)
@@ -203,7 +206,7 @@ with (open('data/sklady_w_zespolach.data', 'w') as f, open('data/zmiany.data', '
 
             #add zmiany
             for zap in zapas_players:
-                print(turn, players)
+                #print(turn, players)
                 changed = random.choice(players)
                 players.remove(changed)
                 zm.write(f"{turn};{changed};{zap};{random.randint(1, 23)}\n")
